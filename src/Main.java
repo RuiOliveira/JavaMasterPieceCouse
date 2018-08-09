b782e59ff92f29b09838afef2789a761a25160e4
OLIVEIRA Rui <ruoliveira@europe.corp.altran.com>
2018-08-09 17:05:07 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        boolean isCar = true;
+        //if isCar = true then true, else false
+        boolean wasCar = isCar ? true : false;
+
+        if(wasCar)
+            System.out.println("wasCar is true");
+    }
+}
